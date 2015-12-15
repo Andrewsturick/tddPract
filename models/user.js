6ae5e9f6f4bc12362e5302d4d63424501bce2065
Ammar <ammar@ammaronline.net>
2015-12-14 17:08:13 -0800

    user model set up

---
+++ models/user.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var User;
+
+var userSchema = Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
+});
+
+User = mongoose.model('User', userSchema);
+
+module.exports = User;
